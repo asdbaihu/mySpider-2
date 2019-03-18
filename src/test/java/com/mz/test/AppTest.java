cc634d2feb02c789c506c51ad3655b2fa175262d
wustmz <865011721@qq.com>
2019-03-18 11:37:00 +0800

    modify

--- src/test/java/com/mz/test/AppTest.java
+++ src/test/java/com/mz/test/AppTest.java
@@ -1,4 +1,4 @@
-package com.mz.day02_job;
+package com.mz.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Day02JobApplicationTests {
+public class AppTest {
 
     @Test
     public void contextLoads() {
