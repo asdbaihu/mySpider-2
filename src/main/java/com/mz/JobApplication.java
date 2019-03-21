7beb303d2ceedc4ca38126af0d0813d825abaacd
wustmz <865011721@qq.com>
2019-03-21 15:42:17 +0800

    modify seed page

--- src/main/java/com/mz/JobApplication.java
+++ src/main/java/com/mz/JobApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling//开启定时任务
+@EnableScheduling
 public class JobApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/mz/test/AppTest.java
+++ src/test/java/com/mz/test/AppTest.java
@@ -1,16 +1,24 @@
 package com.mz.test;
 
+import com.mz.dao.JobInfoDao;
+import com.mz.domain.JobInfo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AppTest {
 
+    @Autowired
+    private JobInfoDao jobInfoDao;
+
     @Test
     public void contextLoads() {
+
     }
 
 }
